a1c9c83545e405d1c2263248f69f2a497aa1375e
Esha-Sherring <esherring2016@gmail.com>
2021-05-01 21:28:11 +0530

    trash routes

--- controllers/sanitationcontrollers.js
+++ controllers/sanitationcontrollers.js
@@ -6,6 +6,7 @@
 
     // * Models
     const Sanitation = require('../models/sanitization');
+    const Trash = require('../models/trash');
 
 
     //Route to register User
@@ -22,6 +23,45 @@
                  
                   const {phone,homeaddress,time,date} = req.body;
                   const newOrder = new Sanitation({
+                   
+                    phone,
+                    homeaddress,
+                    time,
+                    date,
+                   createdBy:req.user.id
+                   
+                  }) ;
+                  console.log(newOrder)
+                  newOrder.save();
+                  
+                
+                
+                  return res.json(newOrder).status(200);
+                }catch(err){
+                  console.log(err)
+                  return res.status(404).send({ error: "server error" });
+                }
+                
+        }
+      }
+
+
+
+
+      //Route to register User
+    //matching passwords handled on the frontend
+    module.exports.trash = async (req, res) => {
+        console.log('trash request route');
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).send('error occured');
+        }
+        else
+        {
+                try{
+                 
+                  const {phone,homeaddress,time,date} = req.body;
+                  const newOrder = new Trash({
                    
                     phone,
                     homeaddress,
--- routes/sanitation.js
+++ routes/sanitation.js
@@ -8,9 +8,11 @@ const router = express.Router();
 // * Controllers
 const sanitationController = require('../controllers/sanitationcontrollers');
 
+
 // * Middlewares
 const { protectUser } = require('../middleware/auth'); 
 
 router.post('/request',[protectUser],sanitationController.request);
+router.post('/trash',[protectUser],sanitationController.trash);
 
 module.exports = router;
